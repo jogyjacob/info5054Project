d879aae2017675dcae1d51683f2d642ecd302db0
PEUROX <pedrofroes@gmail.com>
2019-02-19 13:53:38 -0500

    Add files via upload

---
+++ dbcon.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: trans
+ * Date: 18/02/2019
+ * Time: 22:14
+ */
+	$host = 'localhost';
+	$username = 'londoners';
+	$password = 'London123!';
+	$dbname = 'Londoners';
+	$con = new mysqli($host, $username, $password, $dbname);
+?>
